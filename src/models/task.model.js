816fb107968165de108e4f6dcf771249f3530e0a
santiago-berger <santiago.christian.berger@gmail.com>
2026-08-07 10:57:09 -0300

    feat: se definió el modelo task

---
+++ src/models/task.model.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Task = sequelize.define(
+  "Task",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    isComplete: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: "tasks",
+    timestamps: false,
+  }
+);
