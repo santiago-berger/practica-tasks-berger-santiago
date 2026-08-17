8d6061808676e1b371f634c6faae9f3f490d7f16
santiago-berger <santiago.christian.berger@gmail.com>
2026-08-17 12:45:27 -0300

    fix: se agregó el import de User en task.model.js

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
+import { User } from "./user.model.js";
 
 export const Task = sequelize.define(
   "Task",
