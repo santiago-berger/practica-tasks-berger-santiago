878182d7f81837d47cff02bd1ecc11aca60c0775
santiago-berger <santiago.christian.berger@gmail.com>
2026-08-24 02:30:11 -0300

    feat: se agregaron validaciones de User con express-validator

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,8 +9,8 @@ import {
 
 export const userRouter = Router();
 
-userRouter.post("/users", createUser);       // POST   /api/users
+userRouter.post("/users", createUserValidation, validate, createUser);       // POST   /api/users
 userRouter.get("/users", getUsers);          // GET    /api/users
-userRouter.get("/users/:id", getUserById);    // GET    /api/users/:id
-userRouter.put("/users/:id", updateUser);     // PUT    /api/users/:id
-userRouter.delete("/users/:id", deleteUser);  // DELETE /api/users/:id
+userRouter.get("/users/:id", userIdValidation, validate, getUserById);    // GET    /api/users/:id
+userRouter.put("/users/:id", updateUserValidation, validate, updateUser);     // PUT    /api/users/:id
+userRouter.delete("/users/:id", userIdValidation, validate, deleteUser);  // DELETE /api/users/:id
